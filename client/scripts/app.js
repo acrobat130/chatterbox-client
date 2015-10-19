820580ee8813e9a15d45682b446194e410c28b9f
acrobat130 <shanna.sullivan@gmail.com>
2015-10-19 15:57:37 -0700

    added jquery and ajax methods to app object

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,60 @@
 // YOUR CODE HERE:
+var app = {
+  server: 'https://api.parse.com/1/classes/chatterbox'
+}; 
+app.init = function (){
+
+};
+app.send = function (message){
+  $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: this.server,
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message');
+    }
+  });
+
+};
+
+app.fetch = function(){
+  // $.get('https://api.parse.com/1/classes/chatterbox');
+
+  $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: this.server,
+    type: 'GET',
+    //data: JSON.parse(),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message');
+    }
+  });
+
+
+};
+
+app.clearMessages = function () {
+  $('#chats').children().remove()
+};
+
+app.addMessage = function (messageAdded) {
+  $('#chats').append("<div>" + messageAdded + "</div>");
+
+};
+
+app.addRoom = function (room) {
+  $('#roomSelect').append("<div>" /*+ room + */ +"</div>");
+
+};
 
