cffedd8513382e8d0673fe5bd3fb3e27a47b4313
acrobat130 <shanna.sullivan@gmail.com>
2015-10-19 20:40:25 -0700

    worked on refresh button and fetching data functions in app.js

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -3,12 +3,14 @@ var app = {
   server: 'https://api.parse.com/1/classes/chatterbox'
 }; 
 app.init = function (){
-    $(".username").on('click', app.addFriend);
-    $("#send .submit").on('submit', app.handleSubmit);
+    $(".username").on('click', this.addFriend);
+    $("#send .submit").on('submit', this.handleSubmit);
+    // $('#refreshButton').on('click',this.fetch);
 
 };
 
 app.send = function (message){
+  var thisInstance = this; 
   $.ajax({
     // This is the url you should use to communicate with the parse API server.
     url: this.server,
@@ -16,7 +18,11 @@ app.send = function (message){
     data: JSON.stringify(message),
     contentType: 'application/json',
     success: function (data) {
+      app.addMessage(message);
+      debugger;
+
       console.log('chatterbox: Message sent');
+      console.log('chatterbox', data);
     },
     error: function (data) {
       // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -28,21 +34,31 @@ app.send = function (message){
 
 app.fetch = function(){
   // $.get('https://api.parse.com/1/classes/chatterbox');
-
+  console.log("hello")
   $.ajax({
     // This is the url you should use to communicate with the parse API server.
     url: this.server,
     type: 'GET',
     //data: JSON.parse(),
     contentType: 'application/json',
-    success: function (data) {
-      console.log('chatterbox: Message sent');
+    success: function (data,status, jqxhr) {
+/*      console.log('chatterbox: Request sent');
+      console.log('data', data);
+*/      var userResultsArray = data.results;
+debugger;
+      for (var i = 0; i < userResultsArray.length; i++) {
+        if (userResultsArray[i].username !== undefined && userResultsArray[i].text !== undefined){
+          // debugger; 
+          app.addMessage(userResultsArray[i]);
+        }
+      }
     },
     error: function (data) {
       // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
       console.error('chatterbox: Failed to send message');
     }
   });
+  // debugger;
 
 
 };
@@ -71,8 +87,9 @@ app.addFriend = function(){
 app.handleSubmit = function() {
 
 };
-
 $(document).ready(function(){
   app.init();
+  setInterval(app.fetch(), 1000);
+// $('#refreshButton').on('click', app.fetch);
 });
 
